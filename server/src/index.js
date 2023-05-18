696e4c1d105f123616c5d64726b0c2b5c0f0c050
Sam O'Cain <samocain93@gmail.com>
2023-05-18 14:49:21 -0500

    setting up api and user auth

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import { userRouter } from './routes/users.js';
+
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.listen(3001, () => console.log('Server started on 3001'))
+app.use('/auth', userRouter);
+
+mongoose.connect(
+  'mongodb+srv://samsam123:samsam123@recipes.wxe9zme.mongodb.net/recipes?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log('Server started on 3001'));
