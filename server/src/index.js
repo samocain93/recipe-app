dcb4da757284f5816c1f79ff56fe4183d0a553e5
Sam O'Cain <samocain93@gmail.com>
2023-05-18 15:08:15 -0500

    dotenv added for mongouri

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
 
 import { userRouter } from './routes/users.js';
 
@@ -11,12 +13,9 @@ app.use(cors());
 
 app.use('/auth', userRouter);
 
-mongoose.connect(
-  'mongodb+srv://samsam123:samsam123@recipes.wxe9zme.mongodb.net/recipes?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.listen(3001, () => console.log('Server started on 3001'));
+app.listen(process.env.PORT, () => console.log('Server started on 3001'));
