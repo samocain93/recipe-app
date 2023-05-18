5f670e80c1d259cb1d54a6ecf7d0b3d02fd59a9c
Sam O'Cain <samocain93@gmail.com>
2023-05-18 14:58:33 -0500

    new user registration endpoint successful

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,12 +5,21 @@ import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
   const user = await UserModel.findOne({ username });
 
-  res.json(user);
+  if (user) {
+    return res.json({ message: 'User already exists!' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "New user registered successfully!"});
 });
 
 router.post('/login');
