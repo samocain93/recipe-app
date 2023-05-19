d02844adbaed5076c7f2b9de928caaf12d688583
Sam O'Cain <samocain93@gmail.com>
2023-05-19 18:23:26 -0500

    working on recipes routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 import { userRouter } from './routes/users.js';
+import { recipesRouter } from './routes/recipes.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth', userRouter);
+app.use("/recipes", recipesRouter)
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
