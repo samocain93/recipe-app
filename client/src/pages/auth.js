8176ad87730a1ab6f8f72ba99528a58922910ee3
Sam O'Cain <samocain93@gmail.com>
2023-05-19 18:06:33 -0500

    front end for auth working

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export const Auth = () => {
   return (
@@ -14,7 +16,25 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = async () => {}
+  const [_, setCookies] = useCookies(['access_token']);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', {
+        username,
+        password,
+      });
+
+      setCookies('access_token', response.data.token);
+      window.localStorage.setItem('userID', response.data.userID);
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Form
@@ -35,9 +55,9 @@ const Register = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("http://localhost:3001/auth/register", {
+      await axios.post('http://localhost:3001/auth/register', {
         username,
-        password
+        password,
       });
       alert('Registration completed! Please login.');
     } catch (error) {
